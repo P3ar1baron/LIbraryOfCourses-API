b68ea7642c36a71b9fcfb25ae1964e8a24219c3a
Vlad <v.rozanoff@gmail.com>
2020-05-25 23:24:13 +0300

    Apply returning status codes in get actions

--- LibraryOfCourses/LibraryOfCourses.API/Controllers/AuthorsController.cs
+++ LibraryOfCourses/LibraryOfCourses.API/Controllers/AuthorsController.cs
@@ -20,14 +20,20 @@ namespace LibraryOfCourses.API.Controllers
         public IActionResult GetAuthors()
         {
             var authorsFromRepo = _courseLibraryRepository.GetAuthors();
-            return new JsonResult(authorsFromRepo);
+            return  Ok(authorsFromRepo);
         }
 
         [HttpGet("{authorId:guid}")]
         public IActionResult GetAuthor(Guid authorId)
         {
             var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
-            return new JsonResult(authorFromRepo);
+
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return  Ok(authorFromRepo);
         }
     }
 }
