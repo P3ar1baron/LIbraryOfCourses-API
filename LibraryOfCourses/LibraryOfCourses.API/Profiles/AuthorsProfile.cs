116e53de2bbdb80b2cf28b611f85baedb3e11819
Vlad <v.rozanoff@gmail.com>
2020-05-26 22:08:20 +0300

    Add Author mapping profile

---
+++ LibraryOfCourses/LibraryOfCourses.API/Profiles/AuthorsProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using LibraryOfCourses.API.Helpers;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace LibraryOfCourses.API.Profiles
+{
+    public class AuthorsProfile : Profile
+    {
+        public AuthorsProfile()
+        {
+            CreateMap<CourseLibrary.API.Entities.Author, Models.AuthorDto>()
+                .ForMember(
+                    dest => dest.Name,
+                    opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ForMember(
+                    dest => dest.Age,
+                    opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
+                ;
+        }
+    }
+}
