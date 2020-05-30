a69339c0c127be73b9cf8ac00d57a65cb6affadb
Vlad <v.rozanoff@gmail.com>
2020-05-31 00:46:00 +0300

    Add get and create actions for author collections

--- LibraryOfCourses/LibraryOfCourses.API/Controllers/AuthorCollectionsController.cs
+++ LibraryOfCourses/LibraryOfCourses.API/Controllers/AuthorCollectionsController.cs
@@ -1,10 +1,9 @@
 ﻿using AutoMapper;
 using CourseLibrary.API.Services;
+using LibraryOfCourses.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LibraryOfCourses.API.Controllers
 {
@@ -22,6 +21,34 @@ namespace LibraryOfCourses.API.Controllers
                 throw new ArgumentNullException(nameof(courseLibraryRepository));
             _mapper = mapper ??
                 throw new ArgumentNullException(nameof(mapper));
+        }
+
+        //array key: 1,2,3
+        //composite key: key1=value1, key2=value2
+
+        [HttpGet("({ids})")]
+        public IActionResult GetAuthorCollection(
+            [FromRoute] IEnumerable<Guid> ids)
+        {
+
+        }
+
+
+
+        [HttpPost]
+        public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(
+           IEnumerable<AuthorForCreationDto> authorCollection)
+        {
+            var authorEntities = _mapper.Map<IEnumerable<CourseLibrary.API.Entities.Author>>(authorCollection);
+
+            foreach (var author in authorEntities)
+            {
+                _courseLibraryRepository.AddAuthor(author);
+            }
+
+            _courseLibraryRepository.Save();
+
+            return Ok();
         }
     }
 }
