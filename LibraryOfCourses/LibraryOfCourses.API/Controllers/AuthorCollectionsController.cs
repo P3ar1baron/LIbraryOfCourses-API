91cf571d6f0e537ba1f07894bb640032f0005165
Vlad <v.rozanoff@gmail.com>
2020-05-31 01:36:47 +0300

    Add support of  options

--- LibraryOfCourses/LibraryOfCourses.API/Controllers/AuthorCollectionsController.cs
+++ LibraryOfCourses/LibraryOfCourses.API/Controllers/AuthorCollectionsController.cs
@@ -1,15 +1,17 @@
 ﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
 using CourseLibrary.API.Services;
 using LibraryOfCourses.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LibraryOfCourses.API.Controllers
 {
     [ApiController]
     [Route("api/authorcollections")]
-    public class AuthorCollectionsController
+    public class AuthorCollectionsController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
         private readonly IMapper _mapper;
@@ -26,11 +28,26 @@ namespace LibraryOfCourses.API.Controllers
         //array key: 1,2,3
         //composite key: key1=value1, key2=value2
 
-        [HttpGet("({ids})")]
+        [HttpGet("({ids})", Name = "GetAuthorCollection")]
         public IActionResult GetAuthorCollection(
-            [FromRoute] IEnumerable<Guid> ids)
+            [FromRoute]
+            [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
         {
+            if (ids == null)
+            {
+                return BadRequest();
+            }
 
+            var authorEntities = _courseLibraryRepository.GetAuthors(ids);
+
+            if (ids.Count() != authorEntities.Count())
+            {
+                return NotFound();
+            }
+
+            var authorsToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+
+            return Ok(authorsToReturn);
         }
 
 
@@ -48,6 +65,19 @@ namespace LibraryOfCourses.API.Controllers
 
             _courseLibraryRepository.Save();
 
+            var authorCollectionToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            var idsAsString = string.Join(",",authorCollectionToReturn.Select(a => a.Id));
+
+            return CreatedAtRoute("GetAuthorCollection",
+                new { ids = idsAsString },
+                authorCollectionToReturn);
+        }
+
+        [HttpOptions]
+        
+        public IActionResult GetAuthorsOptions()
+        {
+            Response.Headers.Add("Allow", "GET,OPTIONS,POST");
             return Ok();
         }
     }
