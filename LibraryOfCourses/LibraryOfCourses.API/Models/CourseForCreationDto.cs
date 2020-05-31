77a4a89089d7f73a0f7ee58e23853f1548e5853f
Vlad <v.rozanoff@gmail.com>
2020-05-31 23:43:57 +0300

    Add Validate method in CourseForCreationDto add data annotation attribute to Description

--- LibraryOfCourses/LibraryOfCourses.API/Models/CourseForCreationDto.cs
+++ LibraryOfCourses/LibraryOfCourses.API/Models/CourseForCreationDto.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
 
 namespace LibraryOfCourses.API.Models
 {
-    public class CourseForCreationDto
+    public class CourseForCreationDto : IValidatableObject
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
 
+        [MaxLength(150)]
         public string Description { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield return new ValidationResult(
+                    "The provided description should be different from the title.",
+                    new[] { "CourseForCreationDto" });
+            }
+        }
     }
 }
