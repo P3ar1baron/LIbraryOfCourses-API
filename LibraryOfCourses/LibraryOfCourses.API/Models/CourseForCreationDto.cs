28e05a6f13dbace537f0b2b50b616c61848e9d1f
Vlad <v.rozanoff@gmail.com>
2020-06-03 19:54:59 +0300

    Change port to 081 Add validation attribute class Add error message in validation attributes in dto

--- LibraryOfCourses/LibraryOfCourses.API/Models/CourseForCreationDto.cs
+++ LibraryOfCourses/LibraryOfCourses.API/Models/CourseForCreationDto.cs
@@ -1,26 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using LibraryOfCourses.API.ValidationAttributes;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Security.Cryptography;
 
 namespace LibraryOfCourses.API.Models
 {
-    public class CourseForCreationDto : IValidatableObject
+    [CourseTitleMustBeDifferentFromDescription(
+        ErrorMessage = "Title must be different from a description")]
+    public class CourseForCreationDto //: IValidatableObject
     {
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "You should fill out a title.")]
+        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
         public string Title { get; set; }
 
-        [MaxLength(150)]
+        [MaxLength(500, ErrorMessage = "The description shouldn't have more than 500 characters.")]
         public string Description { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Title == Description)
-            {
-                yield return new ValidationResult(
-                    "The provided description should be different from the title.",
-                    new[] { "CourseForCreationDto" });
-            }
-        }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided description should be different from the title.",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
---
+++ LibraryOfCourses/LibraryOfCourses.API/ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -0,0 +1,22 @@
+﻿using LibraryOfCourses.API.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryOfCourses.API.ValidationAttributes
+{
+    public class CourseTitleMustBeDifferentFromDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value,
+            ValidationContext validationContext)
+        {
+            var course = (CourseForCreationDto)validationContext.ObjectInstance;
+
+            if (course.Title == course.Description)
+            {
+                return new ValidationResult(ErrorMessage,
+                    new[] { nameof(CourseForCreationDto) });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
