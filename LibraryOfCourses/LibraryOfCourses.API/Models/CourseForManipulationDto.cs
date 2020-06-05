3e7294daf333c88f42c87510d254dc155ab67c04
Vlad <v.rozanoff@gmail.com>
2020-06-05 20:41:44 +0300

    Add base course dto

---
+++ LibraryOfCourses/LibraryOfCourses.API/Models/CourseForManipulationDto.cs
@@ -0,0 +1,18 @@
+﻿using LibraryOfCourses.API.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryOfCourses.API.Models
+{
+    [CourseTitleMustBeDifferentFromDescription(
+        ErrorMessage = "Title must be different from a description")]
+    public abstract class CourseForManipulationDto
+    {
+        [Required(ErrorMessage = "You should fill out a title.")]
+        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "You should fill out a description.")]
+        [MaxLength(500, ErrorMessage = "The description shouldn't have more than 500 characters.")]
+        public string Description { get; set; }
+    }
+}
