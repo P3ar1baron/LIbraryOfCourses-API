25ad362fbdec4edd999f76c7e166ce10fd829c46
Vlad <v.rozanoff@gmail.com>
2020-06-05 20:45:41 +0300

    Remove logic from child dtos

--- LibraryOfCourses/LibraryOfCourses.API/Models/CourseForCreationDto.cs
+++ LibraryOfCourses/LibraryOfCourses.API/Models/CourseForCreationDto.cs
@@ -1,28 +1,8 @@
-﻿using LibraryOfCourses.API.ValidationAttributes;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-
-namespace LibraryOfCourses.API.Models
+﻿namespace LibraryOfCourses.API.Models
 {
-    [CourseTitleMustBeDifferentFromDescription(
-        ErrorMessage = "Title must be different from a description")]
-    public class CourseForCreationDto //: IValidatableObject
+
+    public class CourseForCreationDto : CourseForManipulationDto
     {
-        [Required(ErrorMessage = "You should fill out a title.")]
-        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
-        public string Title { get; set; }
-
-        [MaxLength(500, ErrorMessage = "The description shouldn't have more than 500 characters.")]
-        public string Description { get; set; }
-
-        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        //{
-        //    if (Title == Description)
-        //    {
-        //        yield return new ValidationResult(
-        //            "The provided description should be different from the title.",
-        //            new[] { "CourseForCreationDto" });
-        //    }
-        //}
+      
     }
 }
