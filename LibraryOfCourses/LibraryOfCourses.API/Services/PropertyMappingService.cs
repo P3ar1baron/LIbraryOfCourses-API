4ffaeff543e4c3d9a4d8e473e3175ff32685cbd0
vrozanov <vrozanov@atlassian.com>
2020-06-10 17:41:16 +0300

    Add PropertyMappingService and PropertyMappingValue classes

---
+++ LibraryOfCourses/LibraryOfCourses.API/Services/PropertyMappingService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryOfCourses.API.Services
+{
+    public class PropertyMappingService
+    {
+        private Dictionary<string, PropertyMappingValue> _authorPropertyMapping =
+          new Dictionary<string, PropertyMappingValue>(StringComparer.OrdinalIgnoreCase)
+          {
+               { "Id", new PropertyMappingValue(new List<string>() { "Id" } ) },
+               { "MainCategory", new PropertyMappingValue(new List<string>() { "MainCategory" } )},
+               { "Age", new PropertyMappingValue(new List<string>() { "DateOfBirth" } , true) },
+               { "Name", new PropertyMappingValue(new List<string>() { "FirstName", "LastName" }) }
+          };
+    }
+}
